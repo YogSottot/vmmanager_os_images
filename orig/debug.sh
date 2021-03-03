d1fd730c365cc8888969d07c2f7156a0c214f994
YogSottot <7411302+YogSottot@users.noreply.github.com>
2021-03-03 10:45:55 +0300

    Add hook sample

--- orig/debug.sh
+++ orig/debug.sh
@@ -65,14 +65,26 @@ EOF
 ```bash
 cd /opt/
 git clone https://github.com/YogSottot/vmmanager_os_images/
+```
 
-ln -f /opt/vmmanager_os_images/IMG_CentOS-7-amd64_ext4/metainfo.xml /nfsshare/IMG_CentOS-7-amd64_ext4/
-ln -f /opt/vmmanager_os_images/IMG_CentOS-7-amd64_ext4/install.sh /nfsshare/IMG_CentOS-7-amd64_ext4/                                    
+```bash
+# Создаём hook
+cat .git/hooks/post-merge
+#!/bin/sh
 
-ln -f /opt/vmmanager_os_images/IMG_Ubuntu-18.04-amd64/metainfo.xml /nfsshare/IMG_Ubuntu-18.04-amd64/
-ln -f /opt/vmmanager_os_images/IMG_Ubuntu-18.04-amd64/install.sh /nfsshare/IMG_Ubuntu-18.04-amd64/                                                                   
-ln -f /opt/vmmanager_os_images/IMG_Ubuntu-20.04-amd64/metainfo.xml /nfsshare/IMG_Ubuntu-20.04-amd64/
-ln -f /opt/vmmanager_os_images/IMG_Ubuntu-20.04-amd64/install.sh /nfsshare/IMG_Ubuntu-20.04-amd64/                                                                   
-ln -f /opt/vmmanager_os_images/IMG_Debian-10-amd64/metainfo.xml /nfsshare/IMG_Debian-10-amd64/                                                                       
-ln -f /opt/vmmanager_os_images/IMG_Debian-10-amd64/install.sh /nfsshare/IMG_Debian-10-amd64/ 
+ln -f $GIT_DIR/IMG_CentOS-7-amd64_ext4/metainfo.xml /nfsshare/IMG_CentOS-7-amd64_ext4/
+ln -f $GIT_DIR/IMG_CentOS-7-amd64_ext4/install.sh /nfsshare/IMG_CentOS-7-amd64_ext4/
+
+ln -f $GIT_DIR/IMG_Ubuntu-18.04-amd64/metainfo.xml /nfsshare/IMG_Ubuntu-18.04-amd64/
+ln -f $GIT_DIR/IMG_Ubuntu-18.04-amd64/install.sh /nfsshare/IMG_Ubuntu-18.04-amd64/
+
+ln -f $GIT_DIR/IMG_Ubuntu-20.04-amd64/metainfo.xml /nfsshare/IMG_Ubuntu-20.04-amd64/
+ln -f $GIT_DIR/IMG_Ubuntu-20.04-amd64/install.sh /nfsshare/IMG_Ubuntu-20.04-amd64/
+
+ln -f $GIT_DIR/IMG_Debian-10-amd64/metainfo.xml /nfsshare/IMG_Debian-10-amd64/
+ln -f $GIT_DIR/IMG_Debian-10-amd64/install.sh /nfsshare/IMG_Debian-10-amd64/
+```
+
+```bash
+chmod +x .git/hooks/post-merge
 ```
